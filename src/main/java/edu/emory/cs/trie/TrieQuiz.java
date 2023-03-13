59adb7d998c4c0eccd32046aaf0562f9cf88a3f0
Eric Juarez <ejuare2@emory.edu>
2023-03-13 10:29:59 -0400

    Added Trie implementations. Added quiz 5: search a string input for words in a Trie

---
+++ src/main/java/edu/emory/cs/trie/TrieQuiz.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.emory.cs.trie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrieQuiz extends Trie<Integer> {
+    /**
+     * PRE: this trie contains all country names as keys and their unique IDs as values
+     * (e.g., this.get("United States") -> 0, this.get("South Korea") -> 1).
+     * @param input the input string in plain text
+     *              (e.g., "I was born in South Korea and raised in the United States").
+     * @return the list of entities (e.g., [Entity(14, 25, 1), Entity(44, 57, 0)]).
+     */
+    List<Entity> getEntities(String input) {
+        TrieNode<Integer> node = getRoot();
+        List<Entity> entities = new ArrayList<>();
+        // Verify the input or trie is not empty
+        if (node == null || input.length() * node.getChildrenMap().size() == 0)
+            return entities;
+
+        // Two index pointers are used to track the existence of input.substring(s,i) within the trie
+        for (int i = 0, s = -1; i < input.length(); i++) {
+            char c = input.charAt(i);
+            node = node.getChild(c);
+
+            // If the node key is equal to the character and is an endState, a new entity is added,
+            //      node is reset to the root, and s = i to begin a new word
+            if (node != null && node.getKey() == c && node.isEndState()) {
+                entities.add(new Entity(s + 1, i + 1, node.getValue()));
+                node = getRoot();
+                s = i;
+            }
+            // If the node is null or the key is not found, the node is reset to the root and
+            //      s = i to not include the index that didn't contain a character in the input
+            else if (node == null || node.getKey() != c) {
+                node = getRoot();
+                s = i;
+            }
+            // If the node key is equal to the character and is not an endState, s remains fixed while i is incremented
+        }
+        return entities;
+    }
+}
---
+++ src/test/java/edu/emory/cs/trie/TrieQuizTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.cs.trie;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/** @author Jinho D. Choi */
+public class TrieQuizTest {
+    @Test
+    public void testGetEntities1() {
+        final List<String> L = List.of("United States", "South Korea");
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        // String input = "United States";
+        String input = "I was born in South Korea and raised in the United States";
+        List<Entity> entities = List.of(new Entity(44, 57, 0), new Entity(14, 25, 1));
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetEntities2() {
+        final List<String> L = List.of("U");
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        // String input = "United States";
+        String input = "abU";
+        List<Entity> entities = List.of(new Entity(2, 3, 0));
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetEntities3() {
+        final List<String> L = List.of("U");
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        // String input = "United States";
+        String input = "ab";
+        List<Entity> entities = new ArrayList<>();
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetEntities4() {
+        final List<String> L = List.of("United States", "South Korea");
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        // String input = "United States";
+        String input = "South Korea";
+        List<Entity> entities = List.of(new Entity(0, 11, 1));
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetEntities5() {
+        final List<String> L = new ArrayList<>();
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        // String input = "United States";
+        String input = "ab";
+        List<Entity> entities = new ArrayList<>();
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetEntities6() {
+        final List<String> L = List.of("United States", "South Korea");
+        TrieQuiz trie = new TrieQuiz();
+        for (int i = 0; i < L.size(); i++)
+            trie.put(L.get(i), i);
+
+        String input = "South KoreaUnited States";
+        List<Entity> entities = List.of(new Entity(0, 11, 1), new Entity(11, 24, 0));
+        Set<String> expected = entities.stream().map(Entity::toString).collect(Collectors.toSet());
+        Set<String> actual = trie.getEntities(input).stream().map(Entity::toString).collect(Collectors.toSet());
+        assertEquals(expected, actual);
+    }
+}
